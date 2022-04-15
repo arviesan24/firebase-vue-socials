8571fbd57fd47e7ca4732b80d25a2a9f1c752951
Romel Santoral <ctechnology24@gmail.com>
2022-04-15 17:41:08 +0800

    Get email search value

---
+++ src/views/SearchResults.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <div class="card m-5">
+      <div class="card-body">
+        No search found.
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  import { storeToRefs } from 'pinia';
+  import { useUserStore } from '../stores/user';
+
+  const { getemailSearch } = storeToRefs(useUserStore())
+
+</script>
+
+<style scoped>
+  .card {
+    border: 1px solid;
+    padding: 10px;
+    box-shadow: 3px 8px 6px #888888;
+  }
+</style>
